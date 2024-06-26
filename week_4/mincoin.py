12732a879b330efdca7d04fe90251f5229c38414
Jan Poonthong <janpoonthong628@gmail.com>
2024-06-26 15:27:53 +0700

    feat: week_4

--- week_4/mincoin.py
+++ week_4/mincoin.py
@@ -4,21 +4,20 @@ sys.setrecursionlimit(100000)
 
 n = list(map(int, input().split()))
 total = int(input())
-n.sort()
+n = sorted(n, reverse=True)
 
 
-def mincoin(coins, change):
-    min_coin = change
-    if change <= 0:
-        return 0
-    elif change in n:
+def mincoin(v):
+    if v in n:
         return 1
     else:
-        for i in [c for c in n if c <= change]:
-            if i <= change:
-                num_coin = mincoin(coins, change-i)
-                min_coin = min(min_coin, num_coin + 1)
+        s = 0
+        min_coin = v
+        for i in n:
+            if i <= v:
+                s += 1
+                min_coin = min(min_coin, mincoin(v - i) + 1)
     return min_coin
 
 
-print(mincoin(n.sort(), total))
+print(mincoin(total))
